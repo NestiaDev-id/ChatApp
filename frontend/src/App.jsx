6117360b54fca6d7722b2185fb365c99a089c4fe
NestiaDev-id <yohanesdevano90@gmail.com>
2025-02-27 11:58:34 +0700

    add login, register and navbar page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,60 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
+
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+  console.log();
 
   return (
     <div>
-      <p className="text-red-500">hello</p>
-      <button className="btn">Button</button>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        {/* Rute fallback jika tidak ditemukan, redirect ke home */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+
+      <Toaster />
     </div>
   );
 }
---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,60 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSignedUp: false,
+  isLogginIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      // set({ user: res.data.user, isCheckingAuth: false });
+      set({ authUser: res.data });
+    } catch (e) {
+      console.log("Error in checkAuth: " + e);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signUp: async (formData) => {
+    set({ isSignedUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", formData);
+      set({ authUser: res.data });
+      toast.success("Sign up success!");
+    } catch (e) {
+      toast.error(e.response.data.message);
+    } finally {
+      set({ isSignedUp: false });
+    }
+  },
+
+  login: async (formData) => {
+    set({ isLogginIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", formData);
+      set({ authUser: res.data });
+      toast.success("Login success!");
+    } catch (e) {
+      toast.error(e.response.data.message);
+    } finally {
+      set({ isLogginIn: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logout success!");
+    } catch (error) {
+      toast.error("Error logging out");
+    }
+  },
+}));
