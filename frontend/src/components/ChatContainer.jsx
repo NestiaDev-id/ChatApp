6efc77860740b380a63773a2a0e0222eee37aa0b
NestiaDev-id <yohanesdevano90@gmail.com>
2025-03-24 22:49:03 +0700

    fix bug

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,10 +3,14 @@ import { useChatStore } from "../store/useChatStore";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/useAuthStore";
+import { formatMessageTime } from "../lib/utils";
 
 function ChatContainer() {
   const { messages, getMessages, isMessageLoading, selectedUser } =
     useChatStore();
+
+  const { authUser } = useAuthStore();
 
   useEffect(() => {
     if (selectedUser?._id) {
@@ -36,15 +40,15 @@ function ChatContainer() {
           <div
             key={message._id}
             className={`chat ${
-              message.senderId === loggedInUserId ? "chat-end" : "chat-start"
+              message.senderId === authUser._id ? "chat-end" : "chat-start"
             }`}
           >
             <div className="chat-image avatar">
               <div className="size-10 rounded-full border">
                 <img
                   src={
-                    message.senderId === loggedInUserId
-                      ? "/avatar.png" // Atau URL avatar pengguna saat ini
+                    message.senderId === authUser._id
+                      ? authUser?.profilePic || "/avatar.png" // Atau URL avatar pengguna saat ini
                       : selectedUser?.profilePic || "/avatar.png"
                   }
                   alt="profile pic"
@@ -53,7 +57,7 @@ function ChatContainer() {
             </div>
             <div className="chat-header mb-1">
               <time className="text-xs opacity-50 ml-1">
-                {message.createdAt}
+                {formatMessageTime(message.createdAt)}
               </time>
             </div>
             <div className="chat-bubble flex flex-col">
